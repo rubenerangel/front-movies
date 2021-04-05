f714baec511e1a6e4deffa7dc1c1234623f3acaa
Ruben Rangel <rubenrang@gmail.com>
2021-04-05 09:07:18 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MoviesModule } from './movies/movies.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MoviesModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
