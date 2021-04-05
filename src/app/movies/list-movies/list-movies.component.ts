934be84622e4462e87afd18f0fcd63eeea49b9d5
Ruben Rangel <rubenrang@gmail.com>
2021-04-05 16:57:23 -0500

    Change logic to OnInit

--- src/app/movies/list-movies/list-movies.component.ts
+++ src/app/movies/list-movies/list-movies.component.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../../movies.service';
-import {map} from 'rxjs/operators'
-
-interface MoviesType {
-  year: string;
-  title: string;
-  poster: string;
-}
 
 @Component({
   selector: 'app-list-movies',
@@ -15,29 +8,19 @@ interface MoviesType {
 })
 
 export class ListMoviesComponent implements OnInit {
+
   allMovies: any;
   page = 1;
   pageSize = 10;
   collectionSize = 0;
   loading = true;
 
-  constructor(private movies: MoviesService) {
-    this.refreshMovies();
-  }
-
-  refreshMovies() {
-    this.movies.getData().subscribe(data => {
-      console.warn(data);
-      this.loading = false;
-      this.allMovies = data;
-      // data.map()
-    });
-
-    // this.movies.getData().map(res => {
-
-    // })
-  }
+  constructor(private movies: MoviesService ) {}
 
   ngOnInit(): void {
+    this.movies.getData().subscribe((result) => {
+      console.warn('result', result);
+      this.allMovies = result;
+    })
   }
 }
