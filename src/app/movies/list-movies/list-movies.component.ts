d1a9ab95bb7f3d0200dce222c804ca6f85eb27d4
Ruben Rangel <rubenrang@gmail.com>
2021-04-05 19:15:24 -0500

    alt images

--- src/app/movies/list-movies/list-movies.component.ts
+++ src/app/movies/list-movies/list-movies.component.ts
@@ -11,13 +11,16 @@ export class ListMoviesComponent implements OnInit {
 
   allMovies: any;
   p: number = 1;
-  pageSize = 10;
-  collectionSize = 0;
-  loading = true;
+  pageSize:number = 10;
+  loading:boolean = true;
 
   constructor(private movies: MoviesService ) {}
 
   ngOnInit(): void {
+    this.refreshMovies()
+  }
+
+  refreshMovies() {
     this.movies.getData().subscribe((result) => {
       this.loading = false;
       console.warn('result', result);
